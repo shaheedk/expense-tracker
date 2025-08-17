75772421bf5f839cd5f6b6f9c4ae96d907420100
Shaheed mon <sheheedk07@gmail.com>
2025-08-17 12:49:35 +0530

    ongoing commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+import { checkUser } from '@/lib/checkUser'
+const Navbar = () => {
+  const user=checkUser()
+  return (
+    <div>
+      hhh
+    </div>
+  )
+}
+
+export default Navbar
--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-declare global{
-    var prisma:PrismaClient
-}
-export const db=globalThis.prisma||new PrismaClient()
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
 
-if(process.env.NODE_ENV!=='production'){
-    globalThis.prisma=db
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
