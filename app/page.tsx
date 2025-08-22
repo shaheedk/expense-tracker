b893607276cafbd5e6c053e4d5d8d60fcb11a2e2
Shaheed mon <sheheedk07@gmail.com>
2025-08-22 23:46:17 +0530

    added layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
-
-const HomePage = () => {
+import Guest from "@/components/Guest"
+import { currentUser } from "@clerk/nextjs/server"
+const HomePage = async() => {
+  const user=await currentUser()
+  if (!user){
+    return <Guest/>
+  }
   return (
     <div className='bg-red-700' >
       home
